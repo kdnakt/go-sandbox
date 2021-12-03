eb4005142351d6ae4146b7a72f7adcdd5a0e3df1
kdnakt <a.kid.1985@gmail.com>
2021-12-03 21:42:09 +0900

    parse licenses.xml

--- xml1/main.go
+++ xml1/main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"encoding/xml"
+	"io/ioutil"
 )
 
+type LicenseSummary struct {
+	XMLName xml.Name `xml:"licenseSummary"`
+	Dependencies []Dependency `xml:"dependencies>dependency"`
+}
+
+type License struct {
+//	XMLName xml.Name `xml:"license"`
+	Name string `xml:"name"`
+}
+
+type Dependency struct {
+//	XMLName xml.Name `xml:"dependency"`
+	GroupId string `xml:"groupId"`
+	ArtifactId string `xml:"artifactId"`
+	Licenses []License `xml:"licenses>license"`
+}
+
 func main() {
-	fmt.Println("Hello!")
+	data, _ := ioutil.ReadFile("licenses.xml")
+	s := &LicenseSummary{}
+
+	_ = xml.Unmarshal([]byte(data), &s)
+
+	for _, dep := range s.Dependencies {
+		fmt.Println(dep)
+	}
 }
