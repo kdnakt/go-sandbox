131e05a71da742514fb9d08693f611e90fb01433
kdnakt <a.kid.1985@gmail.com>
2021-12-07 18:18:59 +0900

    parse csv

--- xml1/go.mod
+++ xml1/go.mod
@@ -1,3 +1,5 @@
 module github.com/kdnakt/xml1
 
 go 1.17
+
+require github.com/jszwec/csvutil v1.6.0 // indirect
--- xml1/main.go
+++ xml1/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"encoding/xml"
 	"io/ioutil"
+	"strings"
+
+	"github.com/jszwec/csvutil"
 )
 
 type LicenseSummary struct {
@@ -23,35 +27,54 @@ type Dependency struct {
 	ArtifactId string `xml:"artifactId"`
 	Version string `xml:"version"`
 	Licenses []License `xml:"licenses>license"`
+
+	Package string `csv:"module name"`
+	License string `csv:"license"`
+	Repository string `csv:"repository"`
 }
 
 func main() {
-	data, _ := ioutil.ReadFile("licenses.xml")
-	s := &LicenseSummary{}
+	flag.Parse()
+	fileName := flag.Arg(0)
+	data, _ := ioutil.ReadFile(fileName)
 
-	_ = xml.Unmarshal([]byte(data), &s)
-
-	for _, dep := range s.Dependencies {
-		var l License
-		if len(dep.Licenses) > 0 {
-			// TODO: select preferred license
-			l = dep.Licenses[0]
-		} else {
-			// TODO: read license from another list
-			l = License{
-				Name: "NOT FOUND",
-				Url: "NOT FOUND",
+	if strings.HasSuffix(fileName, ".xml") {
+		s := &LicenseSummary{}
+		_ = xml.Unmarshal([]byte(data), &s)
+		for _, dep := range s.Dependencies {
+			var l License
+			if len(dep.Licenses) > 0 {
+				l = dep.Licenses[0]
+			} else {
+				l = License{
+					Name: "NOT FOUND",
+					Url: "NOT FOUND",
+				}
 			}
-		}
-		fmt.Printf(`
+			fmt.Printf(`
 [ライブラリ名] %s
 [ バージョン ] %s
 [ ライセンス ] %s
 [    URL     ] %s
 `,
-			dep.ArtifactId,
-			dep.Version,
-			l.Name,
-			l.Url)
+				dep.ArtifactId,
+				dep.Version,
+				l.Name,
+				l.Url)
+		}
+	} else if strings.HasSuffix(fileName, ".csv") {
+		var deps []Dependency
+		_ = csvutil.Unmarshal([]byte(data), &deps)
+		for _, dep := range deps {
+			p := strings.Split(dep.Package, "@")
+			l := strings.ReplaceAll(dep.License, "*", "")
+			fmt.Printf(`
+[ライブラリ名] %s
+[ バージョン ] %s
+[ ライセンス ] %s
+[    URL     ] %s
+`,
+				p[0], p[1], l, dep.Repository)
+		}
 	}
 }
