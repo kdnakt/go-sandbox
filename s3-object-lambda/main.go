b3e296742ec72493cfba54d49b12f2c5c903d8d5
kdnakt <a.kid.1985@gmail.com>
2023-12-17 23:18:38 +0900

    update

--- s3-object-lambda/main.go
+++ s3-object-lambda/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"context"
-	"net/http"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+type TransformedObject struct {
+	Metadata Metadata `json:"metadata"`
+}
+
+type Metadata struct {
+	Length int    `json:"length"`
+	Md5    string `json:"md5"`
+}
 
 var svc *s3.Client
 
@@ -21,8 +35,8 @@ func init() {
 	svc = s3.NewFromConfig(cfg)
 }
 
-func hello(ctx context.Context, event S3ObjectLambdaEvent) (string, error) {
-	fmt.Printf("[debug] %+v", event) 
+func hello(ctx context.Context, event S3ObjectLambdaEvent) (interface{}, error) {
+	fmt.Printf("[debug] %+v", event)
 	c := event.GetObjectContext
 	fmt.Printf("[%s] %s - %s", c.InputS3Url, c.OutputRoute, c.OutputToken)
 
@@ -36,13 +50,35 @@ func hello(ctx context.Context, event S3ObjectLambdaEvent) (string, error) {
 		return "", err
 	}
 	bodyString := string(bodyBytes)
-	if len(bodyString) > 20 {
-		fmt.Println("Object: ", bodyString[:20])
-	} else {
-		fmt.Println("Object: ", bodyString)
+	fmt.Println("BodyString:")
+	fmt.Println(bodyString)
+
+	transformedObject := TransformedObject{
+		Metadata: Metadata{
+			Length: len(bodyString),
+			Md5:    toMd5(bodyString),
+		},
+	}
+	jsonData, err := json.Marshal(transformedObject)
+	if err != nil {
+		fmt.Println("JSON変換エラー:", err)
+		return nil, err
 	}
 
-	return "Hello λ!", nil
+	input := s3.WriteGetObjectResponseInput{
+		RequestRoute: &c.OutputRoute,
+		RequestToken: &c.OutputToken,
+		Body:         strings.NewReader(string(jsonData)),
+	}
+	return svc.WriteGetObjectResponse(ctx, &input)
+}
+
+func toMd5(data string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(data))
+	hash := hasher.Sum(nil)
+
+	return hex.EncodeToString(hash)
 }
 
 func main() {
@@ -52,57 +88,56 @@ func main() {
 
 // Implementation for aws-lambda-go/events
 type S3ObjectLambdaEvent struct {
-	XAmzRequestId		string			`json:"xAmzrequestId"`
-	GetObjectContext	GetObjectContext	`json:"getObjectContext"`
-	ListObjectsContext	ObjectContext		`json:"listObjectsContext"`
-	ListObjectsV2Context	ObjectContext		`json:"listObjectsV2Context"`
-	HeadObjectContext	ObjectContext		`json:"headObjectContext"`
-	Configuration		Configuration		`json:"configuration"`
-	UserRequest		UserRequest		`json:"userRequest"`
-	UserIdentity		UserIdentity		`json:"userIdentity"`
-	ProtocolVersion		string			`json:"protocolVersion"`
+	XAmzRequestId        string            `json:"xAmzRequestId"`
+	GetObjectContext     *GetObjectContext `json:"getObjectContext,omitempty"`
+	ListObjectsContext   *ObjectContext    `json:"listObjectsContext,omitempty"`
+	ListObjectsV2Context *ObjectContext    `json:"listObjectsV2Context,omitempty"`
+	HeadObjectContext    *ObjectContext    `json:"headObjectContext,omitempty"`
+	Configuration        Configuration     `json:"configuration"`
+	UserRequest          UserRequest       `json:"userRequest"`
+	UserIdentity         UserIdentity      `json:"userIdentity"`
+	ProtocolVersion      string            `json:"protocolVersion"`
 }
 
 type GetObjectContext struct {
-	InputS3Url	string	`json:"inputS3Url"`
-	OutputRoute	string	`json:"outputRoute"`
-	OutputToken	string	`json:"outputToken"`
+	InputS3Url  string `json:"inputS3Url"`
+	OutputRoute string `json:"outputRoute"`
+	OutputToken string `json:"outputToken"`
 }
 
 type ObjectContext struct {
-	InputS3Url	string	`json:"inputS3Url"`
+	InputS3Url string `json:"inputS3Url"`
 }
 
 type Configuration struct {
-	AccessPointArn			string	`json:"accessPointArn"`
-	SupportingAccessPointArn	string	`json:"supportingAccessPointArn"`
-	payload				string	`json:"payload"`
+	AccessPointArn           string `json:"accessPointArn"`
+	SupportingAccessPointArn string `json:"supportingAccessPointArn"`
+	payload                  string `json:"payload"`
 }
 
 type UserRequest struct {
-	url	string			`json:"url"`
-	headers	map[string]string	`json:"headers"`
+	url     string            `json:"url"`
+	headers map[string]string `json:"headers"`
 }
 
 type UserIdentity struct {
-	Type		string		`json:"type"`
-	PrincipalId	string		`json:"principalId"`
-	Arn		string		`json:"arn"`
-	AccountId	string		`json:"accountId"`
-	AccessKeyId	string		`json:"accessKeyId"`
-	SessionContext	SessionContext	`json:"sessionContext"`
+	Type           string          `json:"type"`
+	PrincipalId    string          `json:"principalId"`
+	Arn            string          `json:"arn"`
+	AccountId      string          `json:"accountId"`
+	AccessKeyId    string          `json:"accessKeyId"`
+	SessionContext *SessionContext `json:"sessionContext,omitempty"`
 }
 
 type SessionContext struct {
-	Attributes	map[string]string	`json:"attributes"`
-	SessionIssuer	SessionIssuer		`json:"sessionIssuer"`
+	Attributes    map[string]string `json:"attributes"`
+	SessionIssuer *SessionIssuer    `json:"sessionIssuer,omitempty"`
 }
 
 type SessionIssuer struct {
-	Type		string	`json:"type"`
-	PrincipalId	string	`json:"principalId"`
-	Arn		string	`json:"arn"`
-	AccountId	string	`json:"accountId"`
-	UserName	string	`json:"userName"`
+	Type        string `json:"type"`
+	PrincipalId string `json:"principalId"`
+	Arn         string `json:"arn"`
+	AccountId   string `json:"accountId"`
+	UserName    string `json:"userName"`
 }
-
